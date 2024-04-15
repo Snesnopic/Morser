78eafa83c5cdf015a82e681ec6750ba590ae1876
Snesnopic <snesnopic@gmail.com>
2024-04-15 17:34:13 +0200

    Now plus button works, editing mode replaces texts with textfields

--- Morser/Views/QuickTranslateView.swift
+++ Morser/Views/QuickTranslateView.swift
@@ -11,12 +11,43 @@ import SwiftData
 struct QuickTranslateView: View {
     @Query(sort: \Sentence.order) private var sentences: [Sentence]
     @Environment(\.modelContext) private var modelContext: ModelContext
+    @State private var mode:EditMode = .inactive
+    @State private var createdSentence:String = ""
+    @FocusState private var textFieldIsFocused:Bool
     var body: some View {
         NavigationStack {
             List {
-                ForEach(sentences, id: \.self) {
-                    sentence in
-                    Text(sentence.sentence)
+                ForEach(sentences) { sentence in
+                    if sentence.order == -1 {
+                        TextField("Input",text: $createdSentence)
+                            .focused($textFieldIsFocused)
+                            .onSubmit {
+                                textFieldIsFocused = false
+                                sentence.sentence = createdSentence
+                                createdSentence = ""
+                                let tempItems = sentences
+                                tempItems.indices.forEach({ index in
+                                    sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.order = index
+                                })
+                                do {
+                                    try modelContext.save()
+                                } catch {
+                                    print(error)
+                                }
+                            }
+                    } else if mode.isEditing {
+                        TextField("Input",text: sentence.boundSentence)
+                    }
+                    else {
+                        Text(sentence.sentence)
+                            .onTapGesture {
+                                if !VibrationEngine.shared.isVibrating() {
+                                    VibrationEngine.shared.createEngine()
+                                    VibrationEngine.shared.readMorseCode(morseCode: sentence.morseCode)
+                                }
+                            }
+                        
+                    }
                 }
                 .onDelete { indexSet in
                     indexSet.forEach { index in
@@ -28,9 +59,7 @@ struct QuickTranslateView: View {
                     tempItems.move(fromOffsets: source, toOffset: destination)
                     
                     tempItems.indices.forEach({ index in
-                        print("Indice di \(sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.sentence) prima: \(sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.order)")
                         sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.order = index
-                        print("Indice di \(sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.sentence) dopo: \(sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.order)")
                     })
                     do {
                         try modelContext.save()
@@ -39,7 +68,9 @@ struct QuickTranslateView: View {
                         print(error)
                     }
                 })
+                
             }
+            
             .listStyle(.plain)
             .navigationTitle("Quick Translate")
             .toolbar{
@@ -48,7 +79,8 @@ struct QuickTranslateView: View {
                 }
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
-                        
+                        modelContext.insert(Sentence(sentence: "", order: -1))
+                        textFieldIsFocused = true
                     }
                 label: {
                     Image(systemName: "plus")
@@ -56,10 +88,28 @@ struct QuickTranslateView: View {
                 }
                 
             }
+            .environment(\.editMode, $mode)
         }
     }
 }
 
 #Preview {
-    QuickTranslateView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: Sentence.self, configurations: config)
+    [
+        Sentence(sentence: "Yes, I can guide you.", order: 0),
+        Sentence(sentence: "I'm here to help.", order: 1),
+        Sentence(sentence: "I understand, let me assist you.", order: 2),
+        Sentence(sentence: "I'll write it down for you.", order: 3),
+        Sentence(sentence: "I'll describe the menu options for you.", order: 4),
+        Sentence(sentence: "I'll help you navigate through touch.", order: 5),
+        Sentence(sentence: "I'm communicating with you through touch.", order: 6),
+        Sentence(sentence: "I'll lead you to the bus stop.", order: 7),
+        Sentence(sentence: "Let me describe the location to you.", order: 8),
+        Sentence(sentence: "I'll tap your hand to get your attention.", order: 9)
+    ].forEach { sentence in
+        container.mainContext.insert(sentence)
+    }
+    
+    return QuickTranslateView().modelContainer(container)
 }

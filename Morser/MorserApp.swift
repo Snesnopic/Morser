554f2ef246ecab944285051ab24dd9d14fab1fd4
Snesnopic <snesnopic@gmail.com>
2024-04-15 09:37:02 +0200

    Initial Commit

---
+++ Morser/MorserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorserApp.swift
+//  Morser
+//
+//  Created by Giuseppe Francione on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MorserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

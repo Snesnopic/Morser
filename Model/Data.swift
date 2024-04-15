613c6914bdea93263891b9cc20945b2ed413952b
Snesnopic <snesnopic@gmail.com>
2024-04-15 12:24:40 +0200

    Added models

---
+++ Model/Data.swift
@@ -0,0 +1,21 @@
+//
+//  Data.swift
+//  Morser
+//
+//  Created by Giuseppe Francione on 15/04/24.
+//
+
+import Foundation
+
+var sampleData = [
+   Sentence(sentence: "Yes, I can guide you."),
+   Sentence(sentence: "I'm here to help."),
+   Sentence(sentence: "I understand, let me assist you."),
+   Sentence(sentence: "I'll write it down for you."),
+   Sentence(sentence: "I'll describe the menu options for you."),
+   Sentence(sentence: "I'll help you navigate through touch."),
+   Sentence(sentence: "I'm communicating with you through touch."),
+   Sentence(sentence: "I'll lead you to the bus stop."),
+   Sentence(sentence: "Let me describe the location to you."),
+   Sentence(sentence: "I'll tap your hand to get your attention.")
+]

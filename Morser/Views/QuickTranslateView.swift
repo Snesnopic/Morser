be98b137f6945fc5545c43c9590b01f5e0c1ac87
Snesnopic <snesnopic@gmail.com>
2024-04-15 15:33:23 +0200

    Now displays list of the sentences, can be deleted and re-ordered

--- Morser/Views/QuickTranslateView.swift
+++ Morser/Views/QuickTranslateView.swift
@@ -6,12 +6,56 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct QuickTranslateView: View {
+    @Query(sort: \Sentence.order) private var sentences: [Sentence]
+    @Environment(\.modelContext) private var modelContext: ModelContext
     var body: some View {
         NavigationStack {
-            Text("Hello, World!")
-                .navigationTitle("Quick Translate")
+            List {
+                ForEach(sentences, id: \.self) {
+                    sentence in
+                    Text(sentence.sentence)
+                }
+                .onDelete { indexSet in
+                    indexSet.forEach { index in
+                        modelContext.delete(sentences[index])
+                    }
+                }
+                .onMove(perform: { source, destination in
+                    var tempItems = sentences
+                    tempItems.move(fromOffsets: source, toOffset: destination)
+                    
+                    tempItems.indices.forEach({ index in
+                        print("Indice di \(sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.sentence) prima: \(sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.order)")
+                        sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.order = index
+                        print("Indice di \(sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.sentence) dopo: \(sentences.filter({ $0.sentence == tempItems[index].sentence}).first!.order)")
+                    })
+                    do {
+                        try modelContext.save()
+                    }
+                    catch {
+                        print(error)
+                    }
+                })
+            }
+            .listStyle(.plain)
+            .navigationTitle("Quick Translate")
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        
+                    }
+                label: {
+                    Image(systemName: "plus")
+                }
+                }
+                
+            }
         }
     }
 }

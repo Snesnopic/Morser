8d9257e3c2bad5974e9c8579ebcf8d38a4a12649
Snesnopic <snesnopic@gmail.com>
2024-04-15 11:43:00 +0200

    Editor default text

--- Morser/QuickTranslateView.swift
+++ Morser/QuickTranslateView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct QuickTranslateView: View {
     var body: some View {
         NavigationStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Hello, World!")
                 .navigationTitle("Quick Translate")
         }
     }

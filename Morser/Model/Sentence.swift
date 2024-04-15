759e493cf809efe9c44fb0a7630c0c1c2969fa08
Snesnopic <snesnopic@gmail.com>
2024-04-15 15:32:19 +0200

    Now is a swiftdata model

--- Morser/Model/Sentence.swift
+++ Morser/Model/Sentence.swift
@@ -6,22 +6,17 @@
 //
 
 import Foundation
+import SwiftData
 
-struct Sentence {
+@Model
+class Sentence{
+    init(sentence: String, order: Int) {
+        self.sentence = sentence
+        self.order = order
+    }
     var sentence:String
+    var order:Int
     var morseCode:String {
         return MorseEncoder.encode(string: sentence)
     }
-    static var sampleData = [
-        Sentence(sentence: "Yes, I can guide you."),
-        Sentence(sentence: "I'm here to help."),
-        Sentence(sentence: "I understand, let me assist you."),
-        Sentence(sentence: "I'll write it down for you."),
-        Sentence(sentence: "I'll describe the menu options for you."),
-        Sentence(sentence: "I'll help you navigate through touch."),
-        Sentence(sentence: "I'm communicating with you through touch."),
-        Sentence(sentence: "I'll lead you to the bus stop."),
-        Sentence(sentence: "Let me describe the location to you."),
-        Sentence(sentence: "I'll tap your hand to get your attention.")
-    ]
 }

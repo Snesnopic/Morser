d630b7d09eac5d78ca55086caf421e2248c24bda
Snesnopic <snesnopic@gmail.com>
2024-04-17 09:53:50 +0200

    Better spacing and preview (it's bugged tho)

--- Morser/Views/ParentView.swift
+++ Morser/Views/ParentView.swift
@@ -35,14 +35,14 @@ struct ParentView: View {
                 [
                     Sentence(sentence: "Yes, I can guide you.", order: 0),
                     Sentence(sentence: "I'm here to help.", order: 1),
-                   Sentence(sentence: "I understand, let me assist you.", order: 2),
-                   Sentence(sentence: "I'll write it down for you.", order: 3),
-                   Sentence(sentence: "I'll describe the menu options for you.", order: 4),
-                   Sentence(sentence: "I'll help you navigate through touch.", order: 5),
-                   Sentence(sentence: "I'm communicating with you through touch.", order: 6),
-                   Sentence(sentence: "I'll lead you to the bus stop.", order: 7),
-                   Sentence(sentence: "Let me describe the location to you.", order: 8),
-                   Sentence(sentence: "I'll tap your hand to get your attention.", order: 9)
+                    Sentence(sentence: "I understand, let me assist you.", order: 2),
+                    Sentence(sentence: "I'll write it down for you.", order: 3),
+                    Sentence(sentence: "I'll describe the menu options for you.", order: 4),
+                    Sentence(sentence: "I'll help you navigate through touch.", order: 5),
+                    Sentence(sentence: "I'm communicating with you through touch.", order: 6),
+                    Sentence(sentence: "I'll lead you to the bus stop.", order: 7),
+                    Sentence(sentence: "Let me describe the location to you.", order: 8),
+                    Sentence(sentence: "I'll tap your hand to get your attention.", order: 9)
                 ].forEach { sentence in
                     modelContext.insert(sentence)
                 }
@@ -52,5 +52,8 @@ struct ParentView: View {
 }
 
 #Preview {
-    ParentView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: Sentence.self, configurations: config)
+    
+    return ParentView().modelContainer(container)
 }

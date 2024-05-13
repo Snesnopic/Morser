951c758c6b9baeed8b77db8064118697391edb60
Snesnopic <snesnopic@gmail.com>
2024-05-13 21:49:43 +0200

    New extension

--- Morser/Extensions.swift
+++ Morser/Extensions.swift
@@ -17,3 +17,17 @@ extension View {
         }
     }
 }
+
+extension Character {
+    var isKorean: Bool {
+        // Define the range of Hangul syllables in Unicode
+        let hangulRange = 0xAC00...0xD7A3
+        
+        // Get the Unicode scalar value of the character
+        let scalarValue = self.unicodeScalars.first?.value
+        
+        // Check if the scalar value falls within the Hangul range
+        return scalarValue != nil && hangulRange.contains(Int(scalarValue!))
+    }
+}
+

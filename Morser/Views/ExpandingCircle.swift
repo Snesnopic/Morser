5fcd1073acd9321369f8e8abc5469363f04790b4
Snesnopic <snesnopic@gmail.com>
2024-05-26 20:29:29 +0200

    Now the color is blue by default, and the duration can be given in input but is 0.3 by default

--- Morser/Views/ExpandingCircle.swift
+++ Morser/Views/ExpandingCircle.swift
@@ -10,15 +10,16 @@ import SwiftUI
 struct ExpandingCircle: View {
     @State private var scale: CGFloat = 0.1
     @State private var opacity: Double = 1.0
+    var duration: TimeInterval = 0.3
     @ObservedObject private var vibrationEngine = VibrationEngine.shared
     var body: some View {
         Circle()
-            .fill(vibrationEngine.isVibrating() ? Color.red : .accentColor)
+            .fill(vibrationEngine.isVibrating() ? Color.red : .blue)
             .padding()
             .scaleEffect(scale)
             .opacity(opacity)
             .onAppear {
-                withAnimation(Animation.easeOut(duration: 0.3)) {
+                withAnimation(Animation.easeOut(duration: duration)) {
                     self.scale = 2.0
                     self.opacity = 0.0
                 }

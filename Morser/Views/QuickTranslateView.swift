3bee2c4daa61ed2b42158c7c59d283e020aba0c2
Snesnopic <snesnopic@gmail.com>
2024-05-26 19:19:36 +0200

    swiftdata import was still there lol

--- Morser/Views/QuickTranslateView.swift
+++ Morser/Views/QuickTranslateView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct QuickTranslateView: View {
     @FetchRequest(sortDescriptors: [SortDescriptor(\.order, order: .forward)]) private var sentences: FetchedResults<Sentence>

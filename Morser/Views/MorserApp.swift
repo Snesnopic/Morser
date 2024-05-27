bf98dbe8f3274a1e671c84d93320cace09a8be32
Snesnopic <snesnopic@gmail.com>
2024-05-27 18:15:14 +0200

    Test trying to add intents

--- Morser/Views/MorserApp.swift
+++ Morser/Views/MorserApp.swift
@@ -27,9 +27,29 @@ struct MorserApp: App {
     }
 }
 
+import Intents
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         WatchConnectivityProvider.shared.activate()
         return true
     }
+    func application(_ application: UIApplication, handlerFor intent: INIntent) -> Any? {
+        if intent is EncodeMorseIntent {
+            return EncodeMorseIntentHandler()
+        }
+        return nil
+    }
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if shortcutItem.type == "com.snesnopic.morser" {
+            // Handle the quick action
+            // Present a view controller or encode a predefined text
+            let text = "Sample text"
+            VibrationEngine.shared.createEngine()
+            VibrationEngine.shared.readMorseCode(morseCode: text.morseCode())
+            completionHandler(true)
+        } else {
+            completionHandler(false)
+        }
+    }
+
 }

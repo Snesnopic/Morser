5f0c485e44becb465b0167d62cc77691fc9b1548
Snesnopic <snesnopic@gmail.com>
2024-05-27 08:51:33 +0200

    Now all prints only work in debug

--- Morser/Extensions.swift
+++ Morser/Extensions.swift
@@ -77,3 +77,17 @@ extension View {
         modifier(SizeCalculator(size: size))
     }
 }
+
+public func print(_ object: Any...) {
+#if DEBUG
+    for item in object {
+        Swift.print(item)
+    }
+#endif
+}
+
+public func print(_ object: Any) {
+#if DEBUG
+    Swift.print(object)
+#endif
+}

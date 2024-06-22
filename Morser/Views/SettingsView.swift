9ad0058cacfadc3b57dc602fd2bf022101f3549e
Snesnopic <snesnopic@gmail.com>
2024-06-22 19:43:09 +0200

    A bit better settings view for the mac, but still not ideal

--- Morser/Views/SettingsView.swift
+++ Morser/Views/SettingsView.swift
@@ -14,6 +14,43 @@ struct SettingsView: View {
     @AppStorage("flashlight") private var flashlight = false
 
     @ObservedObject private var vibrationEngine = VibrationEngine.shared
+    private enum flashlightCases {
+        //        case soundOnly
+        case hapticsOnly
+        case soundAndHaptics
+        case disabled
+        case notAvailable
+        var rawValue: String {
+            switch self {
+                //            case .soundOnly:
+                //                return String(localized: "The torch will flash along the sounds")
+            case .hapticsOnly:
+                return String(localized: "The torch will flash along the haptics")
+            case .soundAndHaptics:
+                return String(localized: "The torch will flash along the sounds and haptics")
+            case .disabled:
+                return String(localized: "The torch will not flash")
+            case .notAvailable:
+                return String(localized: "Flashlight not available on this device!")
+            }
+        }
+    }
+    private func whichCase() -> flashlightCases {
+        if !TorchEngine.shared.deviceHasTorch() {
+            return .notAvailable
+        } else if !flashlight {
+            return .disabled
+        } else if soundEnabled && vibrationEngine.supportsHaptics {
+            return .soundAndHaptics
+        } else if !soundEnabled {
+            return .hapticsOnly
+        } else {
+            //            return .soundOnly
+            return .soundAndHaptics
+        }
+    }
+
+#if !os(macOS)
     var body: some View {
         CompatibilityNavigation {
             Form {
@@ -30,9 +67,7 @@ struct SettingsView: View {
                             Text("Sound Pitch (\(Int(soundFrequency)))")
                         } onEditingChanged: { bool in
                             if !bool {
-#if !os(macOS)
                                 WatchConnectivityProvider.sendSettingsToWatch()
-                                #endif
                             }
                         }
                         .disabled(vibrationEngine.isListening || vibrationEngine.isVibrating())
@@ -66,9 +101,7 @@ struct SettingsView: View {
                         Text("Haptics Speed (\(Int(sliderPreference * 100)))")
                     } onEditingChanged: { bool in
                         if !bool {
-#if !os(macOS)
                             WatchConnectivityProvider.sendSettingsToWatch()
-                            #endif
                         }
                     }
                     .disabled(vibrationEngine.isListening || vibrationEngine.isVibrating())
@@ -95,43 +128,95 @@ struct SettingsView: View {
             .navigationTitle(String(localized: "Settings"))
         }
     }
-    private enum flashlightCases {
-//        case soundOnly
-        case hapticsOnly
-        case soundAndHaptics
-        case disabled
-        case notAvailable
-        var rawValue: String {
-            switch self {
-//            case .soundOnly:
-//                return String(localized: "The torch will flash along the sounds")
-            case .hapticsOnly:
-                return String(localized: "The torch will flash along the haptics")
-            case .soundAndHaptics:
-                return String(localized: "The torch will flash along the sounds and haptics")
-            case .disabled:
-                return String(localized: "The torch will not flash")
-            case .notAvailable:
-                return String(localized: "Flashlight not available on this device!")
+#else
+    var body: some View {
+        TabView {
+            VStack {
+                Form {
+                    Section {
+                        Toggle("Sound Haptics", isOn: $soundEnabled)
+                            .disabled(vibrationEngine.isListening || vibrationEngine.isVibrating()/* || !vibrationEngine.supportsHaptics*/)
+                        if soundEnabled {
+                            HStack {
+                                Text("Sound Pitch")
+                                Spacer()
+                                Text("\(Int(soundFrequency))")
+                            }
+                            Slider(value: $soundFrequency, in: 300...800) {
+                                Text("Sound Pitch (\(Int(soundFrequency)))")
+                            }
+                            .disabled(vibrationEngine.isListening || vibrationEngine.isVibrating())
+                        }
+                    } header: {
+                        Text("Sound")
+                    }
+                footer: {
+                    if soundEnabled {
+                        Text("A \(Int(soundFrequency))Hz sound will be played alongside haptics to help recognition.")
+                    } else {
+                        Text("No sound will be played alongside haptics.")
+                    }
+                }
+                }
             }
+            .tabItem { Label("Sound", systemImage: "speaker.wave.3.fill") }
+            .frame(minWidth: 700, minHeight: 350)
+                VStack {
+                    Form {
+                        Section {
+                            Toggle("Flashlight Haptics", isOn: $flashlight)
+                                .disabled(vibrationEngine.isListening || vibrationEngine.isVibrating() || !TorchEngine.shared.deviceHasTorch())
+
+                        } header: {
+                            Text("Flashlight")
+                        } footer: {
+                            Text(whichCase().rawValue)
+                        }
+                    }
+                }
+                .tabItem { Label("Flashlight", systemImage: "flashlight.on.fill") }
+                .frame(minWidth: 350, minHeight: 350)
+            VStack {
+                Form {
+                    Section {
+                        HStack {
+                            Text("Haptics Time Unit")
+                            Spacer()
+                            Text("\(sliderPreference * 100, specifier: "%.0f") ms")
+                        }
+                        Slider(value: $sliderPreference, in: (0.5)...(5.0)) {
+                            Text("Haptics Speed (\(Int(sliderPreference * 100)))")
+                        }
+                        .disabled(vibrationEngine.isListening || vibrationEngine.isVibrating())
+                        .onChange(of: sliderPreference, perform: { _ in
+                            VibrationEngine.shared.updateTimings()
+                        })
+
+                    } header: {
+                        Text("Timings")
+                    }
+                footer: {
+                    Text("""
+                        Time unit is espressed in ms (milliseconds).
+                        Timings:
+                        . = 1 time unit (\(sliderPreference * 100, specifier: "%.0f") ms)
+                        - = 3 time units (\(sliderPreference * 100 * 3, specifier: "%.0f") ms)
+                        Space between same symbols = 1 time unit (\(sliderPreference * 100, specifier: "%.0f") ms)
+                        Space between different symbols = 3 time units (\(sliderPreference * 100 * 3, specifier: "%.0f") ms)
+                        Space between words = 7 time units (\(sliderPreference * 100 * 7, specifier: "%.0f") ms)
+                        """
+                    )
+                }
+                }
+            }
+            .tabItem { Label("Timings", systemImage: "timer") }
+            .frame(minWidth: 700, minHeight: 350)
         }
+
+        .navigationTitle(String(localized: "Settings"))
     }
 
-    private func whichCase() -> flashlightCases {
-        if !TorchEngine.shared.deviceHasTorch() {
-            return .notAvailable
-        } else if !flashlight {
-            return .disabled
-        } else if soundEnabled && vibrationEngine.supportsHaptics {
-            return .soundAndHaptics
-        } else if !soundEnabled {
-            return .hapticsOnly
-        } else {
-//            return .soundOnly
-            return .soundAndHaptics
-        }
-    }
-
+#endif
 }
 
 #Preview {
